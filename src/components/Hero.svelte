c7da432f74bf7500c704ca757191ac9ca0dfec3c
drenfest <drenfest@gmail.com>
2019-08-16 16:11:20 -0500

    Added Sloppy Random Footer Landing Page Links and dynamic landing pages

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -166,8 +166,8 @@
 </section>
 <div class="service-btns-bg">
     <section class="service-buttons">
-        <ServiceBtn serviceBtnBg="#3d5467" serviceLink="/contact" serviceTitle="Schedule Service" serviceIcon="/assets/images/icons/repairs-icon.png" serviceIconAlt="Hvac a/c furnace Repairs Icon">Service</ServiceBtn>
-        <ServiceBtn serviceBtnBg="#324555" serviceLink="/financing" serviceTitle="Get Financing" serviceIcon="/assets/images/icons/replacements-icon.png" serviceIconAlt="Hvac a/c furnace Replacements Icon">Financing</ServiceBtn>
-        <ServiceBtn serviceBtnBg="#607382"  serviceLink="/maintenance" serviceTitle="Setup Annual Maintenance" serviceIcon="/assets/images/icons/maintenance-icon.png" serviceIconAlt="Hvac a/c furnace Maintenance Icon">Maintenance</ServiceBtn>
+        <ServiceBtn serviceBtnBg="#3d5467" serviceLink="/contact" serviceTitle="Schedule Service" serviceIcon="/assets/images/icons/gear.png" serviceIconAlt="Hvac a/c furnace Repairs Icon">Service</ServiceBtn>
+        <ServiceBtn serviceBtnBg="#324555" serviceLink="/financing" serviceTitle="Get Financing" serviceIcon="/assets/images/icons/money.png" serviceIconAlt="Hvac a/c furnace Replacements Icon">Financing</ServiceBtn>
+        <ServiceBtn serviceBtnBg="#607382"  serviceLink="/maintenance" serviceTitle="Setup Annual Maintenance" serviceIcon="/assets/images/icons/calendar.png" serviceIconAlt="Hvac a/c furnace Maintenance Icon">Maintenance</ServiceBtn>
     </section>
 </div>
